5664d0e1d3f36481510ba1fcc6caab6a5745377b
nyrta1 <nurtai_2005@mail.ru>
2024-01-10 04:49:18 +0600

    feat: created authrequest class for get request from form

---
+++ user-service/src/main/java/com/nurik/userservice/models/AuthRequest.java
@@ -0,0 +1,13 @@
+package com.nurik.userservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+    private String username;
+    private String password;
+}
