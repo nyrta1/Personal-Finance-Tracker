0da2d5519a7061a72330963429bb4d09ea9753f8
nyrta1 <nurtai_2005@mail.ru>
2024-01-10 04:50:24 +0600

    perf: annotation improved

--- user-service/src/main/java/com/nurik/userservice/annotations/UsernamePasswordEmailConstraint.java
+++ user-service/src/main/java/com/nurik/userservice/annotations/UsernamePasswordEmailConstraint.java
@@ -2,17 +2,19 @@ package com.nurik.userservice.annotations;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
-import jakarta.validation.constraints.Size;
 
 import java.lang.annotation.*;
 
 @Constraint(validatedBy = {})
-@Size(min = 8, max = 30)
 @Documented
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UsernamePasswordEmailConstraint {
-    String message() default "Length should be between 8 and 30 characters";
+    String message() default "Length should be between {min} and {max} characters";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
+    int minUsernameLength() default 8; // Minimum username length
+    int maxUsernameLength() default 30; // Maximum username length
+    int minPasswordLength() default 8; // Minimum password length
+    int maxPasswordLength() default 30; // Maximum password length
 }
