4507adff45e68b899e7e5f4a29071160aa30c8bf
nyrta1 <nurtai_2005@mail.ru>
2024-01-10 04:49:38 +0600

    fix: rename func names

--- user-service/src/main/java/com/nurik/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/nurik/userservice/repository/UserRepository.java
@@ -1,8 +1,13 @@
 package com.nurik.userservice.repository;
 
-import com.nurik.userservice.models.User;
+import com.nurik.userservice.models.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UserRepository
-        extends JpaRepository<User, Long> {
+        extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByUsername(String username);
 }
