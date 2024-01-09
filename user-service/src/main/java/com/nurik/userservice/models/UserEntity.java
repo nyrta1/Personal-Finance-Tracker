c987952b72f942f063afda0537d3fcde3137038a
nyrta1 <nurtai_2005@mail.ru>
2024-01-10 04:50:45 +0600

    feat: added roles for user entity

--- user-service/src/main/java/com/nurik/userservice/models/UserEntity.java
+++ user-service/src/main/java/com/nurik/userservice/models/UserEntity.java
@@ -2,20 +2,34 @@ package com.nurik.userservice.models;
 
 import com.nurik.userservice.annotations.UsernamePasswordEmailConstraint;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "users")
-public class User {
+public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long userId;
+    private Long id;
 
     @Column(unique = true)
-    @UsernamePasswordEmailConstraint
+    @UsernamePasswordEmailConstraint(minUsernameLength = 8, maxUsernameLength = 30)
     private String username;
 
     @Column(unique = true)
-    @UsernamePasswordEmailConstraint
+    @UsernamePasswordEmailConstraint(minUsernameLength = 8, maxUsernameLength = 30)
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = {CascadeType.ALL, CascadeType.REMOVE})
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id", referencedColumnName = "id")}
+    )
+    private List<Role> roles = new ArrayList<>();
 }
