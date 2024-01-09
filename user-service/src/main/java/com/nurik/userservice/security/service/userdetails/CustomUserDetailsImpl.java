ed7a6826c09afc596959224a91a04bdcdf55c3da
nyrta1 <nurtai_2005@mail.ru>
2024-01-09 23:52:15 +0600

    feat: created custom user details

---
+++ user-service/src/main/java/com/nurik/userservice/security/service/userdetails/CustomUserDetailsImpl.java
@@ -0,0 +1,79 @@
+package com.nurik.userservice.security.service.userdetails;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.nurik.userservice.models.UserEntity;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serial;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Data
+public class CustomUserDetailsImpl implements UserDetails {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String username;
+
+    @JsonIgnore
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public CustomUserDetailsImpl(Long id, String username, String password,
+                                 Collection<? extends GrantedAuthority> authorities) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public static CustomUserDetailsImpl build(UserEntity user) {
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toList());
+
+        return new CustomUserDetailsImpl(
+                user.getId(),
+                user.getUsername(),
+                user.getPassword(),
+                authorities);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+        CustomUserDetailsImpl user = (CustomUserDetailsImpl) obj;
+        return Objects.equals(id, user.id);
+    }
+}
---
+++ user-service/src/main/java/com/nurik/userservice/security/service/userdetails/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.nurik.userservice.security.service.userdetails;
+
+import com.nurik.userservice.models.UserEntity;
+import com.nurik.userservice.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+        return CustomUserDetailsImpl.build(user);
+    }
+}
