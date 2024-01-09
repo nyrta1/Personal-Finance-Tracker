42e236e9fbfbe3d7a2bd1b4bcf10bc2a9f3ee1d8
nyrta1 <nurtai_2005@mail.ru>
2024-01-09 23:51:43 +0600

    feat: created jwt token generator, filter and entry point

---
+++ user-service/src/main/java/com/nurik/userservice/security/jwt/JwtUtils.java
@@ -0,0 +1,64 @@
+package com.nurik.userservice.security.jwt;
+
+import com.nurik.userservice.security.service.userdetails.CustomUserDetailsImpl;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtUtils {
+    private String jwtSecret;
+    private int jwtExpirationMs;
+
+    public String generateJwtToken(Authentication authentication) {
+        CustomUserDetailsImpl userPrincipal = (CustomUserDetailsImpl) authentication.getPrincipal();
+        Date issuedAt = new Date();
+
+        return Jwts.builder()
+                .setSubject(userPrincipal.getUsername())
+                .setIssuedAt(issuedAt)
+                .setExpiration(new Date(issuedAt.getTime() + jwtExpirationMs))
+                .signWith(key(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUserNameFromJwtToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                    .build()
+                .parseClaimsJwt(token)
+                    .getBody()
+                .getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                        .build()
+                    .parse(authToken);
+            return true;
+        } catch (MalformedJwtException e) {
+            log.error("Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            log.error("JWT token is expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            log.error("JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims string is empty: {}", e.getMessage());
+        }
+
+        return false;
+    }
+}
