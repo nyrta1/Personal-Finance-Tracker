7e5d1cfdc7cdd8cb791e9acc08859e49f55825fc
nyrta1 <nurtai_2005@mail.ru>
2024-01-11 03:37:01 +0600

    feat: added discovery client for eureka server

--- balance-service/src/main/java/com/nurik/balanceservice/BalanceServiceApplication.java
+++ balance-service/src/main/java/com/nurik/balanceservice/BalanceServiceApplication.java
@@ -2,12 +2,12 @@ package com.nurik.balanceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BalanceServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BalanceServiceApplication.class, args);
 	}
-
 }
