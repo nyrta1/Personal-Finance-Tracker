a03765ec4d8c254522e232d7736b99af0b476f91
nyrta1 <nurtai_2005@mail.ru>
2024-01-10 07:10:49 +0600

    feat: changed structure of this annotation and removed max length limit

--- user-service/src/main/java/com/nurik/userservice/annotations/UsernamePasswordEmailConstraint.java
+++ user-service/src/main/java/com/nurik/userservice/annotations/UsernamePasswordEmailConstraint.java
@@ -2,19 +2,17 @@ package com.nurik.userservice.annotations;
 
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
+import jakarta.validation.constraints.Size;
 
 import java.lang.annotation.*;
 
 @Constraint(validatedBy = {})
+@Size(min = 8)
 @Documented
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface UsernamePasswordEmailConstraint {
-    String message() default "Length should be between {min} and {max} characters";
+    String message() default "Length should be more than 8 characters";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
-    int minUsernameLength() default 8; // Minimum username length
-    int maxUsernameLength() default 30; // Maximum username length
-    int minPasswordLength() default 8; // Minimum password length
-    int maxPasswordLength() default 30; // Maximum password length
 }
