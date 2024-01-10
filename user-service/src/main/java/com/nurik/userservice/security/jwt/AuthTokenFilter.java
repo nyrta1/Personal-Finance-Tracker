1edf99ae76532f7487af82136df882987e1beab8
nyrta1 <nurtai_2005@mail.ru>
2024-01-10 07:16:07 +0600

    fix: log error fixed

--- user-service/src/main/java/com/nurik/userservice/security/jwt/AuthTokenFilter.java
+++ user-service/src/main/java/com/nurik/userservice/security/jwt/AuthTokenFilter.java
@@ -45,7 +45,7 @@ public class AuthTokenFilter extends OncePerRequestFilter {
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         } catch (Exception e) {
-            log.error("Cannot set user authentication: {}", e);
+            log.error("Cannot set user authentication: {}", e.getMessage());
         }
 
         filterChain.doFilter(request, response);
