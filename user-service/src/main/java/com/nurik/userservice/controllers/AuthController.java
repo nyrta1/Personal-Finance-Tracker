66924db614e263039c47a4b015410b58acd54d03
nyrta1 <nurtai_2005@mail.ru>
2024-01-10 07:21:36 +0600

    feat: authentication controller is done. Ready to use!

--- user-service/src/main/java/com/nurik/userservice/controllers/AuthController.java
+++ user-service/src/main/java/com/nurik/userservice/controllers/AuthController.java
@@ -2,14 +2,15 @@ package com.nurik.userservice.controllers;
 
 import com.nurik.userservice.exception.UserNotFoundException;
 import com.nurik.userservice.models.AuthRequest;
+import com.nurik.userservice.models.MessageResponse;
 import com.nurik.userservice.models.UserEntity;
-import com.nurik.userservice.security.jwt.JwtUtils;
-import com.nurik.userservice.service.userservice.UserService;
+import com.nurik.userservice.service.authservice.AuthService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -18,61 +19,44 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/users/auth")
 @Slf4j
 public class AuthController {
-    private final JwtUtils jwtUtils;
-    private final UserService userService;
-    private final AuthenticationManager authenticationManager;
+    private final AuthService authService;
 
     @Autowired
-    public AuthController(JwtUtils jwtUtils, UserService userService, AuthenticationManager authenticationManager) {
-        this.jwtUtils = jwtUtils;
-        this.userService = userService;
-        this.authenticationManager = authenticationManager;
-    }
-
-    @GetMapping("/hello")
-    public ResponseEntity<HttpStatus> hello() {
-        return new ResponseEntity<>(HttpStatus.OK);
+    public AuthController(AuthService authService) {
+        this.authService = authService;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<HttpStatus> loginUser(@RequestBody AuthRequest authUser) {
-        log.error("HELLO FROM LOGIN POST");
+    public ResponseEntity<?> loginUser(@RequestBody AuthRequest authUser) {
         try {
             if (authUser.getUsername() == null || authUser.getPassword() == null) {
                 throw new UserNotFoundException("UserName or Password is Empty");
             }
-            UserEntity userData = userService.getUserEntity(authUser);
-            if (userData == null) {
-                throw new UserNotFoundException("UserName or Password is Invalid");
-            }
-            Authentication authentication = getAuthentication(userData);
-            String authJwtToken = jwtUtils.generateJwtToken(authentication);
-            return new ResponseEntity<>(HttpStatus.OK);
+            ResponseCookie jwtCookie = authService.authenticateUser(authUser);
+            return ResponseEntity.ok()
+                    .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
+                    .body("Successfully!");
         } catch (UserNotFoundException e) {
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body("Username or password is invalid!");
         }
     }
 
     @PostMapping("/register")
-    public ResponseEntity<HttpStatus> registerUser(@RequestBody AuthRequest authUser) {
-        log.error("HELLO FROM REGISTER POST");
+    public ResponseEntity<?> registerUser(@RequestBody AuthRequest authUser) {
         try {
             if (authUser.getUsername() == null || authUser.getPassword() == null) {
                 throw new UserNotFoundException("UserName or Password is Empty");
             }
-            UserEntity userData = userService.getUserEntity(authUser);
-            if (userData != null) {
-                return new ResponseEntity<>(HttpStatus.CONFLICT);
-            }
-            userService.registerTheUser(authUser);
-            return new ResponseEntity<>(HttpStatus.CREATED);
+            MessageResponse statusResponse = authService.registerUser(authUser);
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .body(statusResponse.getMessage());
         } catch (UserNotFoundException e){
-            return new ResponseEntity<>(HttpStatus.CONFLICT);
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body("Username or password is invalid!");
         }
-    }
-
-    private Authentication getAuthentication(UserEntity user) {
-        return authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword()));
     }
 }
