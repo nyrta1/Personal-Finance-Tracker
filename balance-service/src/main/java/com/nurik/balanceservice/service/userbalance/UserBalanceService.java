0118449db884a1bf4eaff662555de3898d5f8a19
nyrta1 <nurtai_2005@mail.ru>
2024-01-11 03:36:07 +0600

    feat: created user balance service for controller

---
+++ balance-service/src/main/java/com/nurik/balanceservice/service/userbalance/UserBalanceService.java
@@ -0,0 +1,7 @@
+package com.nurik.balanceservice.service.userbalance;
+
+import com.nurik.balanceservice.model.UserBalance;
+
+public interface UserBalanceService {
+    void create(Long userId);
+}
---
+++ balance-service/src/main/java/com/nurik/balanceservice/service/userbalance/UserBalanceServiceImpl.java
@@ -0,0 +1,26 @@
+package com.nurik.balanceservice.service.userbalance;
+
+import com.nurik.balanceservice.model.UserBalance;
+import com.nurik.balanceservice.repository.UserBalanceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+class UserBalanceServiceImpl implements UserBalanceService {
+    private final UserBalanceRepository userBalanceRepository;
+
+    @Autowired
+    public UserBalanceServiceImpl(UserBalanceRepository userBalanceRepository) {
+        this.userBalanceRepository = userBalanceRepository;
+    }
+
+    @Override
+    public void create(Long userId) {
+        UserBalance userBalance = UserBalance.builder()
+                .userId(userId)
+                .totalMoney(0L)
+                .build();
+
+        userBalanceRepository.save(userBalance);
+    }
+}
