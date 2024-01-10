4cb2f486569470eb203511642c52456e39841f8f
nyrta1 <nurtai_2005@mail.ru>
2024-01-11 03:38:39 +0600

    fix: changed method connection of the two microservices

--- auth-service/src/main/java/com/nurik/userservice/client/BalanceClient.java
+++ auth-service/src/main/java/com/nurik/userservice/client/BalanceClient.java
@@ -1,13 +1,11 @@
 package com.nurik.userservice.client;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
 @HttpExchange
 public interface BalanceClient {
-    @PostExchange("/balance/auth")
-    ResponseEntity<?> authTheUser(@RequestBody Authentication authentication);
+    @PostExchange("/balance/user/create")
+    void createBalance(@RequestBody Long userId);
 }
