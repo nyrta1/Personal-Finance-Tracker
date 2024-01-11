505afa59bb67750198cf050193737cef7e52d3ef
nyrta1 <nurtai_2005@mail.ru>
2024-01-12 02:23:54 +0600

    feat: added new post method to validate jwt token from auth-service microservice

--- balance-service/src/main/java/com/nurik/balanceservice/controller/UserBalanceController.java
+++ balance-service/src/main/java/com/nurik/balanceservice/controller/UserBalanceController.java
@@ -1,24 +1,25 @@
 package com.nurik.balanceservice.controller;
 
+import com.nurik.balanceservice.client.AuthClient;
 import com.nurik.balanceservice.service.userbalance.UserBalanceService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/balance/user")
 @Slf4j
 public class UserBalanceController {
     private final UserBalanceService userBalanceService;
+    private final AuthClient authClient;
 
     @Autowired
-    public UserBalanceController(UserBalanceService userBalanceService) {
+    public UserBalanceController(UserBalanceService userBalanceService, AuthClient authClient) {
         this.userBalanceService = userBalanceService;
+        this.authClient = authClient;
     }
 
     @PostMapping("/create")
@@ -26,5 +27,13 @@ public class UserBalanceController {
         userBalanceService.create(userId);
         log.info("The registered userId is: {}", userId);
         return ResponseEntity.status(HttpStatus.OK).body("The registered userId is: " + userId);
+    }
+
+    @PostMapping("/jwt/validate")
+    public ResponseEntity<?> validate(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader) {
+        Long userId = authClient.getUserIdFromJwt(authorizationHeader);
+        return ResponseEntity
+                .status(userId == null ? HttpStatus.OK : HttpStatus.UNAUTHORIZED)
+                .body(userId);
     }
 }
