f41e23f497c5941425c78ace3255b50c255e9ad5
nyrta1 <nurtai_2005@mail.ru>
2024-01-12 04:17:31 +0600

    feat: added method findByUserId to find UserBalance by userId

--- balance-service/src/main/java/com/nurik/balanceservice/repository/UserBalanceRepository.java
+++ balance-service/src/main/java/com/nurik/balanceservice/repository/UserBalanceRepository.java
@@ -4,7 +4,10 @@ import com.nurik.balanceservice.model.UserBalance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserBalanceRepository
         extends JpaRepository<UserBalance, Long> {
+    Optional<UserBalance> findByUserId(Long userId);
 }
