bf8844e35914ad8bf8deb0de2d4c0f797433cfbc
nyrta1 <nurtai_2005@mail.ru>
2024-01-12 03:57:24 +0600

    perf: improved structure of class

--- auth-service/src/main/java/com/nurik/userservice/security/whitelist/HttpRequestWhiteList.java
+++ auth-service/src/main/java/com/nurik/userservice/security/whitelist/HttpRequestWhiteList.java
@@ -1,20 +1,40 @@
 package com.nurik.userservice.security.whitelist;
 
+import java.util.Arrays;
+
 public class HttpRequestWhiteList {
-    public final static String[] POST_AUTH_REQUEST = {
-            "/users/auth/login",
-            "/users/auth/register",
-    };
+    public static class AUTH_REQUEST {
+        public final static String[] GET = {};
+        public final static String[] POST = {
+                "/users/auth/login",
+                "/users/auth/register",
+        };
 
-    public final static String[] POST_BALANCE_MICROSERVICE = {
-            "/users/auth/jwt-token-valid",
-    };
+        public final static String[] PUT = {};
+        public final static String[] DELETE = {};
+    }
 
-    public final static String[] GET_ACTUATOR_INFO = {
-            "/actuator",
-            "/actuator/health/{*path}",
-            "/actuator/health",
-            "/actuator/info",
-            "/actuator/prometheus"
-    };
+    public static class BALANCE_MICROSERVICE {
+        public final static String[] GET = {};
+        public final static String[] POST = {
+                "/users/auth/jwt/extract-userid",
+        };
+
+        public final static String[] PUT = {};
+        public final static String[] DELETE = {};
+    }
+
+    public static class ACTUATOR_INFO {
+        public final static String[] GET = {
+                "/actuator",
+                "/actuator/health/{*path}",
+                "/actuator/health",
+                "/actuator/info",
+                "/actuator/prometheus"
+        };
+        public final static String[] POST = {};
+
+        public final static String[] PUT = {};
+        public final static String[] DELETE = {};
+    }
 }
