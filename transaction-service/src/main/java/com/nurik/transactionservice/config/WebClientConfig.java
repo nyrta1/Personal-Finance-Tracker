fca690a298322ad0e69aecbeefa30dcbce0f6043
nyrta1 <nurtai_2005@mail.ru>
2024-01-12 15:49:35 +0600

    feat: added config to load balancer filter config

---
+++ transaction-service/src/main/java/com/nurik/transactionservice/config/WebClientConfig.java
@@ -0,0 +1,35 @@
+package com.nurik.transactionservice.config;
+
+import com.nurik.transactionservice.client.BalanceClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.reactive.LoadBalancedExchangeFilterFunction;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.web.server.WebFilterChainProxy;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+@Configuration
+public class WebClientConfig {
+    @Autowired
+    private LoadBalancedExchangeFilterFunction filterFunction;
+
+    @Bean
+    public WebClient balanceWebClient() {
+        return WebClient.builder()
+                .baseUrl("http://balance-service")
+                .filter(filterFunction)
+                .build();
+    }
+
+    @Bean
+    public BalanceClient balanceClient() {
+        HttpServiceProxyFactory httpServiceProxyFactory
+                = HttpServiceProxyFactory
+                .builder(WebClientAdapter.forClient(balanceWebClient()))
+                .build();
+
+        return httpServiceProxyFactory.createClient(BalanceClient.class);
+    }
+}
