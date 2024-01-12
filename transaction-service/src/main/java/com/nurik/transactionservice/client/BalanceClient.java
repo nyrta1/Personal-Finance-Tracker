7fcce73a7348ffc613761703e9287bafebbcfd8c
nyrta1 <nurtai_2005@mail.ru>
2024-01-13 03:43:18 +0600

    feat: added new two methods to communcate with balance-service

--- transaction-service/src/main/java/com/nurik/transactionservice/client/BalanceClient.java
+++ transaction-service/src/main/java/com/nurik/transactionservice/client/BalanceClient.java
@@ -1,13 +1,20 @@
 package com.nurik.transactionservice.client;
 
 import com.nurik.transactionservice.model.UserBalance;
-import org.springframework.http.HttpHeaders;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
 
 @HttpExchange
 public interface BalanceClient {
     @GetExchange("/balance/user")
-    UserBalance getUserBalance(@RequestHeader HttpHeaders headers);
+    UserBalance getUserBalance(@RequestParam("userId") Long userId);
+
+    @PostExchange("/balance/user/spend")
+    Boolean spend(@RequestParam("userId") Long userId, @RequestParam("amount") Integer amount);
+
+    @PostExchange("/balance/user/add")
+    Boolean add(@RequestParam("userId") Long userId, @RequestParam("amount") Integer amount);
 }
