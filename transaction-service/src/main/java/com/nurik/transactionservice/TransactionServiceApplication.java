c3f10ecf41cc5411ded0a5dff0bbac2a107021f5
nyrta1 <nurtai_2005@mail.ru>
2024-01-12 15:51:11 +0600

    refactor: removed useless spaces

--- transaction-service/src/main/java/com/nurik/transactionservice/TransactionServiceApplication.java
+++ transaction-service/src/main/java/com/nurik/transactionservice/TransactionServiceApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class TransactionServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TransactionServiceApplication.class, args);
 	}
-
 }
