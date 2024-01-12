4d87d2a32be26aace5968200155737015277d376
nyrta1 <nurtai_2005@mail.ru>
2024-01-13 03:42:16 +0600

    feat: added authClient to communicate with between two microservices

---
+++ transaction-service/src/main/java/com/nurik/transactionservice/client/AuthClient.java
@@ -0,0 +1,11 @@
+package com.nurik.transactionservice.client;
+
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.HttpExchange;
+import org.springframework.web.service.annotation.PostExchange;
+
+@HttpExchange
+public interface AuthClient {
+    @PostExchange("/users/auth/jwt/extract-userid")
+    Long getUserIdFromJwt(@RequestParam("jwtToken") String jwtToken);
+}
