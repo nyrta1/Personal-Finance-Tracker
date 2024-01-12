017a5345b7df773d92d99bc7e5a29240e9277e68
nyrta1 <nurtai_2005@mail.ru>
2024-01-13 03:44:15 +0600

    feat: added new /create method to create a transaction

--- transaction-service/src/main/java/com/nurik/transactionservice/controller/TransactionController.java
+++ transaction-service/src/main/java/com/nurik/transactionservice/controller/TransactionController.java
@@ -1,34 +1,65 @@
 package com.nurik.transactionservice.controller;
 
+import com.nurik.transactionservice.client.AuthClient;
 import com.nurik.transactionservice.client.BalanceClient;
+import com.nurik.transactionservice.model.TransactionStatus;
+import com.nurik.transactionservice.model.Transactions;
 import com.nurik.transactionservice.model.UserBalance;
+import com.nurik.transactionservice.payload.request.TransactionRequest;
+import com.nurik.transactionservice.service.transactionservice.TransactionService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transaction")
 @Slf4j
 public class TransactionController {
     private final BalanceClient balanceClient;
+    private final AuthClient authClient;
+    private final TransactionService transactionService;
 
     @Autowired
-    public TransactionController(BalanceClient balanceClient) {
+    public TransactionController(BalanceClient balanceClient, AuthClient authClient, TransactionService transactionService) {
         this.balanceClient = balanceClient;
+        this.authClient = authClient;
+        this.transactionService = transactionService;
     }
 
     @PostMapping("/user/balance")
-    public ResponseEntity<UserBalance> getUserBalance(@RequestHeader HttpHeaders headers) {
-        UserBalance userBalance = balanceClient.getUserBalance(headers);
+    public ResponseEntity<UserBalance> getUserBalance(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader) {
+        Long currentUserId = authClient.getUserIdFromJwt(authorizationHeader);
+        UserBalance userBalance = balanceClient.getUserBalance(currentUserId);
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(userBalance);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Transactions> getTransaction(@RequestHeader(HttpHeaders.AUTHORIZATION) String authorizationHeader, @RequestBody TransactionRequest transactionRequest) {
+        Long currentUserId = authClient.getUserIdFromJwt(authorizationHeader);
+        UserBalance userBalance = balanceClient.getUserBalance(currentUserId);
+        if (!getStatusFromBalanceService(currentUserId, transactionRequest.getAmount(), transactionRequest.getStatus())) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(null);
+        }
+        Transactions transactions = transactionService.save(userBalance, transactionRequest);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(transactions);
+    }
+
+    private boolean getStatusFromBalanceService(Long currentUserId, Integer amount, TransactionStatus status) {
+        if (status == TransactionStatus.ADD) {
+            return balanceClient.add(currentUserId, amount);
+        }
+        if (status == TransactionStatus.SPEND) {
+            return balanceClient.spend(currentUserId, amount);
+        }
+        return false;
     }
 }
