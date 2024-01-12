1e1a201beac67301097279c595698f77ba5e0551
nyrta1 <nurtai_2005@mail.ru>
2024-01-12 22:22:09 +0600

    feat: added new model category

---
+++ transaction-service/src/main/java/com/nurik/transactionservice/model/Category.java
@@ -0,0 +1,23 @@
+package com.nurik.transactionservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @Column(name = "category_name", nullable = false, length = 50)
+    private String categoryName;
+}
