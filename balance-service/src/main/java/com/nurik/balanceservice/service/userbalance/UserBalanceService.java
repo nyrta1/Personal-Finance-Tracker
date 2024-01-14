499776c3c806a996c766b03e2f1fb1482da36f9b
nyrta1 <nurtai_2005@mail.ru>
2024-01-15 01:05:04 +0600

    feat: added spend and add methods for controller

--- balance-service/src/main/java/com/nurik/balanceservice/service/userbalance/UserBalanceService.java
+++ balance-service/src/main/java/com/nurik/balanceservice/service/userbalance/UserBalanceService.java
@@ -1,8 +1,11 @@
 package com.nurik.balanceservice.service.userbalance;
 
 import com.nurik.balanceservice.model.UserBalance;
+import org.springframework.http.HttpStatus;
 
 public interface UserBalanceService {
     void create(Long userId);
     UserBalance findByUserId(Long userId);
+    Boolean spent(Long userId, Integer amount);
+    Boolean add(Long userId, Integer amount);
 }
--- balance-service/src/main/java/com/nurik/balanceservice/service/userbalance/UserBalanceServiceImpl.java
+++ balance-service/src/main/java/com/nurik/balanceservice/service/userbalance/UserBalanceServiceImpl.java
@@ -3,6 +3,7 @@ package com.nurik.balanceservice.service.userbalance;
 import com.nurik.balanceservice.model.UserBalance;
 import com.nurik.balanceservice.repository.UserBalanceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -27,5 +28,36 @@ class UserBalanceServiceImpl implements UserBalanceService {
     @Override
     public UserBalance findByUserId(Long userId) {
         return userBalanceRepository.findByUserId(userId).orElse(null);
+    }
+
+    @Override
+    public Boolean spent(Long userId, Integer amount) {
+        UserBalance userBalance = userBalanceRepository.findByUserId(userId).orElse(null);
+        if (userBalance == null) {
+            return false;
+        }
+        if (userBalance.getTotalMoney() < amount) {
+            return false;
+        }
+
+        Long resultRemainsMoney = userBalance.getTotalMoney() - amount;
+        userBalance.setTotalMoney(resultRemainsMoney);
+        userBalanceRepository.save(userBalance);
+
+        return true;
+    }
+
+    @Override
+    public Boolean add(Long userId, Integer amount) {
+        UserBalance currentUserBalance = userBalanceRepository.findByUserId(userId).orElse(null);
+        if (currentUserBalance == null) {
+            return false;
+        }
+
+        Long resultRemainsMoney = currentUserBalance.getTotalMoney() + amount;
+        currentUserBalance.setTotalMoney(resultRemainsMoney);
+        userBalanceRepository.save(currentUserBalance);
+
+        return true;
     }
 }
