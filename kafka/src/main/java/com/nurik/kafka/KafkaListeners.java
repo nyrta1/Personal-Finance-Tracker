2c0507eb8af212c6714d3198744e89c6acc09ca1
nyrta1 <nurtai_2005@mail.ru>
2024-01-20 00:04:14 +0600

    feat: created kafka listener

---
+++ kafka/src/main/java/com/nurik/kafka/KafkaListeners.java
@@ -0,0 +1,14 @@
+package com.nurik.kafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+    @KafkaListener(
+            topics = "personal-finance-tracker-topic",
+            groupId = "groupId"
+    )
+    void listener(String data) {
+        System.out.println("Listener received: " + data + " 🎉");
+    }
+}
