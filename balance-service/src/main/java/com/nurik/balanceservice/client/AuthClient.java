0896a2e1b8de97ca3f2f379b7a576727dc31accc
nyrta1 <nurtai_2005@mail.ru>
2024-01-20 21:26:51 +0600

    refactor: removed unused imports

--- balance-service/src/main/java/com/nurik/balanceservice/client/AuthClient.java
+++ balance-service/src/main/java/com/nurik/balanceservice/client/AuthClient.java
@@ -1,9 +1,5 @@
 package com.nurik.balanceservice.client;
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
